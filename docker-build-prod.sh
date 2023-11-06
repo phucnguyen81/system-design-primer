ca74c1d94576f6556a23c2cf44d43774fe1d75cb
Phuc K Nguyen <phucknguyen@gmail.com>
2023-11-06 15:17:51 +0000

    Replace destination in prod build

---
+++ docker-build-prod.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+docker build -t system-design-primer .
+
+docker run \
+    -v .:/app \
+    --workdir /app \
+    -e JEKYLL_ENV=production \
+    system-design-primer \
+    bundle exec jekyll build -d ./docs
---
+++ docs/docker-attach.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker exec -it system-design-primer bash
